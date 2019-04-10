6f83ec6bccc5490dc59cad8781c12ca2b8a2e5dd
Naga krushna Yelisetty <ynagakrushna@gmail.com>
2019-04-10 15:38:24 +0530

    Added chain validation and  replacing the smallest chains with longest chains

---
+++ blockchain.js
@@ -0,0 +1,68 @@
+const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
+
+class Blockchain {
+  constructor() {
+    // First block in chain is genesis block
+    this.chain = [Block.genesis()];
+  }
+
+  // Add a new block to chain 
+  addBlock({ data }) {
+    const newBlock = Block.mineBlock({
+      lastBlock: this.chain[this.chain.length-1],
+      data
+    });
+
+    this.chain.push(newBlock);
+  }
+
+  // Valid chain method
+  static isValidChain(chain) {
+
+    // Testing whether chain is started with genesis block.
+    if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    // check lastHash Reference with in current block is valid or not
+    for(let i=1; i<chain.length; i++) {
+      // Get the block
+      const { timestamp, lastHash, hash, data } = chain[i];
+
+      // take previous block hash
+      const actualLastHash = chain[i-1].hash;
+
+      // test lastHash
+      if(lastHash !== actualLastHash) return false;
+
+      // Test any fields has changed by creating a hash again for the data
+      const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+      // Test both hashes.
+      if(hash !== validatedHash) return false;
+    }
+
+    // It is a valid chain.
+    return true;
+  }
+
+  replaceChain(chain) {
+
+    if(chain.length <= this.chain.length) {
+      console.error('The incoming chain must be longer');
+      return;
+    }
+
+    if (!Blockchain.isValidChain(chain)) {
+      console.error('The incoming chain must be valid.');
+      return;
+    }
+
+    console.log('replacing chain with', chain);
+    this.chain = chain;
+  }
+
+}
+
+module.exports = Blockchain;
