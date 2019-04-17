66abe6fac3f57ad6417089bc1b9e629cb0b2f56d
Naga krushna Yelisetty <ynagakrushna@gmail.com>
2019-04-17 15:46:34 +0530

    Working with different instances of the blockchain with different ports

---
+++ app/pubsub.js
@@ -0,0 +1,59 @@
+const redis = require('redis');
+
+const CHANNELS = {
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN'
+};
+
+class PubSub {
+  constructor({ blockchain }) {
+    this.blockchain = blockchain;
+
+    this.publisher = redis.createClient();
+    this.subscriber = redis.createClient(); // object
+
+    this.subscribeToChannels();
+
+    this.subscriber.on(
+      'message',
+      (channel, message) => this.handleMessage(channel, message)
+    );
+  }
+
+  handleMessage(channel, message) {
+    console.log(`Message recieved. Channel: ${channel}. Message: ${message}`);
+
+    const parsedMessage = JSON.parse(message);
+
+    if (channel === CHANNELS.BLOCKCHAIN) {
+      this.blockchain.replaceChain(parsedMessage);
+    }
+  }
+
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach(channel => {
+      this.subscriber.subscribe(channel);
+    });
+  }
+
+  // publisher can not get published message, so unsubscribe from the channel first and then publish it and again subscribe to channel
+  publish({ channel, message }) {
+    this.subscriber.unsubscribe(channel, () => {
+      this.publisher.publish(channel, message, () => {
+        this.subscriber.subscribe(channel);
+      });
+    });
+  }
+
+  broadcastChain() {
+    // Local function
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    });
+  }
+
+}
+
+
+module.exports = PubSub;
